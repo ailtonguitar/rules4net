8a77a46ac4b9ca8a174e6280c5a9ea2b97a3309b
Ailton pinto <Ailton pinto>
2016-12-17 00:32:52 -0200

    Equals constraint tests file.

---
+++ Rules4Net.Tests/Constraints/EqualsConstraintTests.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Rules4Net.Tests.Constraints
+{
+    [TestClass]
+    public class EqualsConstraintTests
+    {
+    }
+}
