30e7e92b1f9c2d0cf1351e237a6b14ccdd4e487b
Ailton Pinto <ailtonguitar@hotmail.com>
2016-12-20 16:36:12 -0200

    Including startswith constraint.

---
+++ Rules4Net.Tests/Constraints/StartsWithConstraintTests.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Rules4Net.Data;
+using Rules4Net.Data.Constraints;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Rules4Net.Tests.Constraints
+{
+    [TestClass]
+    public class StartsWithConstraintTests : TestBase
+    {
+        [TestMethod]
+        public void ShouldBePossibleEvaluateRuleWithStartsWithConstraint()
+        {
+            var rule = new Rule();
+            var filter = rule.AddAndFilter();
+            filter.Add(new StartsWithConstraint("Name", "John"));
+
+            this.AddRule(rule);
+
+            var rules = this.Engine.Evaluate(new
+            {
+                Name = "John Doe"
+            });
+
+            Assert.AreEqual(1, rules.Count());
+        }
+
+        [TestMethod]
+        public void ShouldNotBePossibleEvaluateRuleWithStartsWithConstraintAndOtherStartSubstring()
+        {
+            var rule = new Rule();
+            var filter = rule.AddAndFilter();
+            filter.Add(new StartsWithConstraint("Name", "John"));
+
+            this.AddRule(rule);
+
+            var rules = this.Engine.Evaluate(new
+            {
+                Name = "ohn"
+            });
+
+            Assert.AreEqual(0, rules.Count());
+        }
+    }
+}
---
+++ Rules4Net/Data/Constraints/StartsWithConstraint.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Rules4Net.Data.Constraints
+{
+    public class StartsWithConstraint : ExpressionConstraint
+    {
+        public StartsWithConstraint(string property, string value)
+            : base(property, (o) =>
+            {
+                return o.ToString().StartsWith(value);
+            }) { }
+    }
+}
