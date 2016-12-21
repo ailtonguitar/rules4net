c1f5bc91dbea4288e77f2596397d11dd76571254
jplindgren <joaopozo@gmail.com>
2016-12-21 16:09:42 -0200

    Remove duplicated test

--- Rules4Net.Tests/Constraints/EqualsConstraintTests.cs
+++ Rules4Net.Tests/Constraints/EqualsConstraintTests.cs
@@ -1,35 +1,13 @@
 ﻿using Rules4Net.Data;
 using Rules4Net.Data.Constraints;
-using Rules4Net.Engine;
-using Rules4Net.Repository;
 using Rules4Net.Tests.TestObjects.Financial;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using Xunit;
 
-namespace Rules4Net.Tests.Constraints
-{    
-    public class EqualsConstraintTests : TestBase
-    {
-        [Fact]
-        public void ShouldBePossibleEvaluateRuleWithEqualsConstraint()
-        {
-            var rule = new Rule();
-            var filter = rule.AddAndFilter();
-            filter.Add(new EqualsConstraint("Name", "John Doe"));
-
-            this.AddRule(rule);
-
-            var data = new Dictionary<string, object>();
-            data["Name"] = "John Doe";
-
-            var rules = this.Engine.Evaluate(data);
-
-            Assert.Equal(1, rules.Count());
-        }
-
+namespace Rules4Net.Tests.Constraints {
+    public class EqualsConstraintTests : TestBase {
         [Fact]
         public void ShouldNotBePossibleEvaluateRuleWithEqualsConstraintAndNotEqualValue()
         {
