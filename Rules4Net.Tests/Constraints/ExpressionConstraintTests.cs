683459aa342ec06eee199d8230bc8019d84153f8
jplindgren <joaopozo@gmail.com>
2016-12-22 19:18:22 -0200

    Add Generic ExpressionConstraint

--- Rules4Net.Tests/Constraints/ExpressionConstraintTests.cs
+++ Rules4Net.Tests/Constraints/ExpressionConstraintTests.cs
@@ -1,5 +1,6 @@
 ﻿using Rules4Net.Data;
 using Rules4Net.Data.Constraints;
+using Rules4Net.Tests.TestObjects.Financial;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,28 +12,40 @@ namespace Rules4Net.Tests.Constraints
     public class ExpressionConstraintTests : TestBase
     {
         [Fact]
-        public void ShouldBePossibleEvaluateRuleWithExpressionConstraint()
-        {
-            var rule = new Rule();
-            var filter = rule.AddAndFilter();
+        public void ShouldEvaluateWithUntypedExpression() {
+            var data = new Dictionary<string, object> { { "Name", "Jane Doe" } };            
 
-            filter.Add(new ExpressionConstraint("Name", (o) => {
+            var sut = new ExpressionConstraint("Name", (o) => {
                 return o.ToString().Length > 5;
-            }));
+            });
 
-            this.AddRule(rule);
-
-            var data = new Dictionary<string, object>();
-            data["Name"] = "Jane Doe";
-
-            var rules = this.Engine.Evaluate(data);
-
-            Assert.Equal(1, rules.Count());
+            Assert.True(sut.Evaluate(data));            
 
             data["Name"] = "Jane";
+            Assert.False(sut.Evaluate(data));
+        }        
+
+        [Fact]
+        public void ShouldEvaluateWithTypedExpression() {
+            var data = new Dictionary<string, object>() { { "Debt", new Money(10.0m, Currency.REAL) } };            
+
+            var sut = new ExpressionConstraint<Money>("Debt", (o) => {
+                return o.Amount > 12.3m;
+            });
+
+            Assert.False(sut.Evaluate(data));
             
-            rules = this.Engine.Evaluate(data);
-            Assert.Equal(0, rules.Count());
+            sut = new ExpressionConstraint<Money>("Debt", (o) => {
+                return o.Amount > 9.3m;
+            });
+            Assert.True(sut.Evaluate(data));
         }
-    }
+
+        [Fact]
+        public void ShouldEvaluateAsFalseIfExpressionIsNull() {
+            var data = new Dictionary<string, object>() { { "Name", "Jhon Doe" } };
+            var sut = new ExpressionConstraint<Money>("Debt", null);
+            Assert.False(sut.Evaluate(data));
+        }
+    } //class
 }
