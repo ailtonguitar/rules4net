383ce05dcfec17bc9194225bafd4c03e8b57f703
jplindgren <jplindgrenbr@hotmail.com>
2016-12-22 23:13:51 -0200

    Fix name ConcurrentBagHelper to ConcurrentBagExntesions

--- Rules4Net/Extensions/ConcurrentBagExtensions.cs
+++ Rules4Net/Extensions/ConcurrentBagExtensions.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 
 namespace Rules4Net.Extensions {
-    public static class ConcurrentBagHelper {
+    public static class ConcurrentBagExtensions {
         public static void AddRange<T>(this ConcurrentBag<T> @this, IEnumerable<T> toAdd) {
             foreach (var element in toAdd) {
                 @this.Add(element);
