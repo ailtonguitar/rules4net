e84176ad3ca0ba797ecdb9a96b84915a8a69b006
Ailton Pinto <ailtonguitar@hotmail.com>
2016-12-27 16:52:31 -0200

    Adding new IsTodayConstraint and tests

---
+++ Rules4Net.Tests/Constraints/IsTodayConstraintTests.cs
@@ -0,0 +1,40 @@
+﻿using Rules4Net.Data;
+using Rules4Net.Data.Constraints;
+using Rules4Net.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace Rules4Net.Tests.Constraints
+{
+    public class IsTodayConstraintTests : TestBase
+    {
+        [Fact]
+        public void ShouldBePossibleEvaluateRuleWithIsTodayConstraint()
+        {
+            var rule = new Rule();
+            var filter = rule.AddAndFilter();
+            filter.Add(new IsTodayConstraint("Birthday"));
+
+            this.AddRule(rule);
+
+            var rules = this.Engine.Evaluate(new
+            {
+                Birthday = DateTime.Now
+            });
+
+            Assert.Equal(1, rules.Count());
+        }
+
+        [Fact]
+        public void ShouldNotBePossibleEvaluateRuleWithIsTodayConstraintWhenDateIsNotToday()
+        {
+            var data = new { Birthday = DateTime.Now.AddDays(-10) };
+            var constraint = new IsTodayConstraint("Birthday");
+            var result = constraint.Evaluate(ObjectHelper.ToDictionary(data));
+            Assert.False(result);
+        }
+    }
+}
---
+++ Rules4Net/Data/Constraints/IsTodayConstraint.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Rules4Net.Data.Constraints
+{
+    public class IsTodayConstraint : IConstraint
+    {
+        private string _property;
+
+        public IsTodayConstraint(string property)
+        {
+            this._property = property;
+        }
+
+        public virtual bool Evaluate(IDictionary<string, object> data)
+        {
+            if (!data.ContainsKey(_property) || !(data[_property] is DateTime))
+                return false;
+
+            var value = (DateTime)data[_property];
+            return value.Date.Equals(DateTime.Today);
+        }
+    }
+}
