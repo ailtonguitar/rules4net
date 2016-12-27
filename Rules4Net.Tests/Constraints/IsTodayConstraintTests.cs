21b91439a2fe228505af1ebcd258025f96de15fb
Ailton Pinto <ailtonguitar@hotmail.com>
2016-12-27 18:21:59 -0200

    Changing to use utc dates in IsTodayConstraint and adding more tests

--- Rules4Net.Tests/Constraints/IsTodayConstraintTests.cs
+++ Rules4Net.Tests/Constraints/IsTodayConstraintTests.cs
@@ -29,6 +29,23 @@ namespace Rules4Net.Tests.Constraints
         }
 
         [Fact]
+        public void ShouldBePossibleEvaluateRuleWithIsUtcTodayConstraint()
+        {
+            var rule = new Rule();
+            var filter = rule.AddAndFilter();
+            filter.Add(new IsTodayConstraint("Birthday"));
+
+            this.AddRule(rule);
+
+            var rules = this.Engine.Evaluate(new
+            {
+                Birthday = DateTime.UtcNow
+            });
+
+            Assert.Equal(1, rules.Count());
+        }
+
+        [Fact]
         public void ShouldNotBePossibleEvaluateRuleWithIsTodayConstraintWhenDateIsNotToday()
         {
             var data = new { Birthday = DateTime.Now.AddDays(-10) };
@@ -42,6 +59,15 @@ namespace Rules4Net.Tests.Constraints
         public void ShouldNotBePossibleEvaluateRuleWithIsTodayConstraintWhenPropertyIsMissing()
         {
             var data = new { };
+            var constraint = new IsTodayConstraint("Birthday");
+            var result = constraint.Evaluate(ObjectHelper.ToDictionary(data));
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void ShouldNotBePossibleEvaluateRuleWithIsTodayConstraintWhenPropertyIsNotADate()
+        {
+            var data = new { Birthday = 10 };
             var constraint = new IsTodayConstraint("Birthday");
             var result = constraint.Evaluate(ObjectHelper.ToDictionary(data));
             Assert.False(result);
