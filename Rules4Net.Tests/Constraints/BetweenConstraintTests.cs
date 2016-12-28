5cb3dd8ceba74a941d4571187c4d584b4f9465d6
Ailton Pinto <ailtonguitar@hotmail.com>
2016-12-28 13:28:04 -0200

    Adding new BetweenConstraint and unit tests

---
+++ Rules4Net.Tests/Constraints/BetweenConstraintTests.cs
@@ -0,0 +1,43 @@
+﻿using Rules4Net.Data;
+using Rules4Net.Data.Constraints;
+using Rules4Net.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace Rules4Net.Tests.Constraints
+{
+    public class BetweenConstraintTests : TestBase
+    {
+        [Fact]
+        public void ShouldBePossibleEvaluateRuleWithBetweenConstraint()
+        {
+            var rule = new Rule();
+            var filter = rule.AddAndFilter();
+            filter.Add(new BetweenConstraint("Age", 12, 18));
+
+            this.AddRule(rule);
+
+            var rules = this.Engine.Evaluate(new
+            {
+                Age = 13
+            });
+
+            Assert.Equal(1, rules.Count());
+        }
+
+        [Fact]
+        public void ShouldNotBePossibleEvaluateRuleWithBetweenConstraintAndValueOutOfRange()
+        {
+            var data = ObjectHelper.ToDictionary(new
+            {
+                Age = 19
+            });
+
+            var constraint = new BetweenConstraint("Age", 12, 18);
+            Assert.False(constraint.Evaluate(data));
+        }
+    }
+}
---
+++ Rules4Net/Data/Constraints/BetweenConstraint.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Rules4Net.Data.Constraints
+{
+    public class BetweenConstraint : IConstraint
+    {
+        private GreaterOrEqualConstraint lowerBound;
+        private LessOrEqualConstraint upperBound;
+
+        public BetweenConstraint(string property, object lower, object upper)
+        {
+            this.lowerBound = new GreaterOrEqualConstraint(property, lower);
+            this.upperBound = new LessOrEqualConstraint(property, upper);
+        }
+
+        public bool Evaluate(IDictionary<string, object> data)
+        {
+            return this.lowerBound.Evaluate(data) && this.upperBound.Evaluate(data);
+        }
+    }
+}
